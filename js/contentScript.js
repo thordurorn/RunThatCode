748120120a259795ad483a2b4c1b1c2e432aaf39
thordurorn <github@thorduorn.net>
2013-06-08 16:40:17 +0000

    <code> tag now gets parsed into an DTO on icon click

--- js/contentScript.js
+++ js/contentScript.js
@@ -9,6 +9,21 @@ runThatCodeIcon.attr('id', function(i) {
 		return 'runThatCodeIconId_'+(i+1);
 });
 
+function parseCodeElement(inElem){
+	theRslt = {};
+	theClass = "-Unknown";
+	theClasses = (inElem.parent('pre').attr("class"));
+	if(theClasses !== undefined)
+		theClass = (theClasses.split(" "))[0];
+	theRslt.language = theClass.substring(theClass.search("-") + 1);
+	theRslt.code = inElem.text(); //.replace(/<\/?[^>]+>/gi, '');
+	thePos = inElem.offset();
+	theRslt.left = thePos.left;
+	theRslt.top = thePos.top;
+	return theRslt;
+}
+
+
 $(runThatCodeIcon).click(function(event) {
 	var runThatCodeIconId = event.target.id;
 	console.log('Info: A RunThatCode icon was pressed.');
@@ -16,6 +31,7 @@ $(runThatCodeIcon).click(function(event) {
 
 	var runThatCodeSnippetId = runThatCodeIconId.replace('runThatCodeIconId', 'runThatCodeSnippetId');
 	console.log('Info: runThatCodeSnippetId is ' + runThatCodeSnippetId);
-	var codeSnippet = $('#' + runThatCodeSnippetId).contents().text();
-	console.log(codeSnippet);
+	var codeSnippet = $('#' + runThatCodeSnippetId);
+	var codeElementDescription = parseCodeElement(codeSnippet);
+	console.log(codeElementDescription);
 });
