ae1c2c296e8b5e3eb1dc4f77ba2d085ee93400c0
unknown <finnbjorn@Developement.family.home>
2013-06-08 18:01:45 +0000

    Bætti við jQuery UI dialog

--- js/contentScript.js
+++ js/contentScript.js
@@ -24,14 +24,30 @@ function parseCodeElement(inElem){
 	return theRslt;
 }
 
+$("body").append('<div id="dialog" title="Results">'+
+	'<p id="runThisCodeDialogMessage"></p>' +
+	'</div>');
+
 $(runThatCodeIcon).click(function(event) {
 	var runThatCodeIconId = event.target.id;
 	console.log('Info: A RunThatCode icon was pressed.');
-	console.log('Info: RunThatCode icon is ' + runThatCodeIconId);
+	console.log('Info: RunThatCode icon is "' + runThatCodeIconId + '"');
 
 	var runThatCodeSnippetId = runThatCodeIconId.replace('runThatCodeIconId', 'runThatCodeSnippetId');
-	console.log('Info: runThatCodeSnippetId is ' + runThatCodeSnippetId);
+	console.log('Info: runThatCodeSnippetId is "' + runThatCodeSnippetId + '"');
 	var codeSnippet = $('#' + runThatCodeSnippetId);
 	var codeElementDescription = parseCodeElement(codeSnippet);
-	console.log(codeElementDescription);
+	console.log('Info: Code sent to ideone is "' + codeElementDescription + '"') ;
+
+	$("#runThisCodeDialogMessage").empty();
+	$("#runThisCodeDialogMessage").append(codeElementDescription.code);
+	$("#dialog").dialog(
+		{ buttons: [{
+			text: "Ok",
+			click: function() { $( this ).dialog( "close" ); }
+		}]},
+		{ width: "90%" },
+		{ height: "auto" },
+		{ modal: true }
+	);
 });
