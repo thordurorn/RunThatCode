675f56fb6cde19e88fb55ecc7766abe54e879b10
thordurorn <github@thorduorn.net>
2013-06-10 19:15:24 +0000

    Added some show/hide gimmicks, code clean up and commenting

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,5 +1,6 @@
 var codeSection = $('pre').children('code');
-var rtcLogo = "http://i41.tinypic.com/33mc50w.png"
+//var rtcLogo = "http://i41.tinypic.com/33mc50w.png";
+var rtcLogo = "http://i43.tinypic.com/311mjwj.png";
 var rtcSpinnerUrl = "http://i39.tinypic.com/2cmof9t.gif";  //chrome.extension.getURL("/img/spinnerLarge.gif");
 var codeSectionId = "";
 
@@ -15,35 +16,16 @@ runThatCodeIcon.attr('id', function(i) {
 
 $("body").append('<div id="dialog" title="Results">'+
 	'<div id="runThisCodeDialogMessage"></div>' +
-	'<img src="' + rtcSpinnerUrl + '" id="runThatCodeSpinnerImage"/>' +
+	//'<img src="' + rtcSpinnerUrl + '" id="runThatCodeSpinnerImage"/>' +
 	'</div>' +
-	'<div id="languageSelection"></div>');
-$("#runThatCodeSpinnerImage").hide();
+	'<div id="languageSelection" class="rtcDspNone"></div>');
 
 var languageSelection = $('#languageSelection');
 
-function langToIdeone(lang){
-	lang.toLowerCase();
-	//TODO: add code that parses for spaces
-	var dict = {}; 
-	     //dict["awk"]= 104; dict["bash"]= 28;
-	     dict["c"]= 11; dict["cs"]= 27; dict["c++"]= 34; dict["objective-c"]= 34;
-	     dict["java"]= 10; dict["java7"]= 55; dict["javascript"]= 35;
-	     //dict["pascal"]= 2; 
-	     //dict["perl"]= 3; dict["perl6"]= 54;
-	     dict["php"]= 29;
-	     dict["python"]= 4; dict["python3"]=  116;
-	     dict["vb.net"]= 101;
-	     dict["sql"]= 40;
-	return dict[lang];
-}
-
-function getLangList(){
-
-}
-
+/**
+*	Performs an HTTP Post containing the code to ideone.com 
+**/
 function postAjax(inInfo){
-	//var langCode = langToIdeone(inInfo.language);
 	var langCode = $("#languageSelection option:selected").val();
 	console.log('Info: Selected language code is "' + langCode + '"');
 	$.ajax({
@@ -63,28 +45,13 @@ function postAjax(inInfo){
 						var stuff = getLink(inData,'<input type="text" id="link_presentation" value="');
 						console.log("Info: link_presentation value is '" + stuff + "'");
   						window.open(stuff, '_blank');
-
-  						/* This code may be used in the netxt version:
-  						var getDiv = function(inData, startTag){
-  							start = inData.indexOf(startTag);
-  							stop = inData.indexOf('</div>', start);
-  							return (inData.substring(start, stop + 6)).trim();
-  						}
-
-  						//Todo: filter out the relevant html and remove ALOT of gunk
-  						theInfo = getDiv(inData, '<div id="info" class="view_box');
-  						theCode = getDiv(inData, '<div id="code" class="view_box');
-  						theErr = getDiv(inData, '<div id="err" class="view_box');
-  						theInOutErr = getDiv(inData, '<div id="inouterr" class="view_box');
-  						
-  						$("#runThatCodeSpinnerImage").hide();
-  						$("#runThisCodeDialogMessage").append(theInfo, theCode, theErr, theInOutErr);
-  						*/
   					}
-  		//,dataType: 'multipart/formdata'
 	});
 }
 
+/**
+*	Parses all usefull info from the SO <code> Element into an DTO
+**/
 function parseCodeElement(inElem){
 	theRslt = {};
 	theClass = "-Unknown";
@@ -100,14 +67,18 @@ function parseCodeElement(inElem){
 	return theRslt;
 }
 
+/**
+*	Event handler for  icon clicks
+**/
 $(runThatCodeIcon).click(function(event) {
     $(languageSelection).empty().hide().append(rtcLangDDwn);
 
 	var thisPos = $(this).position();
 	var thisHeight = $(this).height();
 	var thisWidth = $(this).width();
+	$(this).hide();
 	var ddnlistWidth = $(languageSelection).width();
-	var ddnListTop = thisPos.top + thisHeight;
+	var ddnListTop = thisPos.top; // + thisHeight;
 	var ddnListLeft = thisPos.left - ddnlistWidth + thisWidth;
 
 	$(languageSelection)
@@ -118,31 +89,32 @@ $(runThatCodeIcon).click(function(event) {
 	    	0,
 	    	function(){}
     	)
-    	.show();
+    	.show().attr('size',8);
 
 	codeSectionId = $(this).attr('id');
-	console.log("Info: codeSectionId is '" + codeSectionId +"'");
+	//console.log("Info: codeSectionId is '" + codeSectionId +"'");
 });
 
+/**
+*	Event handler for selection, there is only one selection
+**/
 $(languageSelection).change(function(event) {
 	var runThatCodeIconId = codeSectionId; //event.target.id;
 	var runThatCodeSnippetId = runThatCodeIconId.replace('runThatCodeIconId', 'runThatCodeSnippetId');
 	//console.log('Info: runThatCodeSnippetId is "' + runThatCodeSnippetId + '"');
 	var codeSnippet = $('#' + runThatCodeSnippetId);
 	var codeElementDescription = parseCodeElement(codeSnippet);
-	console.log('Language:'  + (codeElementDescription.language));
-	console.log('TranslatesTo:'  + langToIdeone(codeElementDescription.language));
+	//console.log('Language:'  + (codeElementDescription.language));
+	//console.log('TranslatesTo:'  + langToIdeone(codeElementDescription.language));
 	$("#runThisCodeDialogMessage").empty();
 	$("#runThatCodeSpinnerImage").show();
 	postAjax(codeElementDescription);
-	/*$("#dialog").dialog(
-		{ buttons: [{
-			text: "Close",
-			click: function() { $( this ).dialog( "close" ); }
-		}]},
-		{ width: "90%" },
-		{ height: "auto" },
-		{ modal: true }
-	);*/
 	$(languageSelection).empty().hide();
+	$("img.runThatCodeIcon").show();
+
+});
+
+$('#languageSelection').mouseleave(function(e){
+	$('#languageSelection').hide();
+	$("img.runThatCodeIcon").show();
 });
